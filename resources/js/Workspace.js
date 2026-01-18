268cc9d9dca956aa7dd8aa2c3f22880a28086f98
Filip Wallberg <wallberg@gmail.com>
2026-01-18 20:14:12 +0100

    Revert "Update Workspace.js"
    This reverts commit 37a370b8c6188201c3f8be7c41c81c3ff27f6d95.

--- resources/js/Workspace.js
+++ resources/js/Workspace.js
@@ -427,12 +427,7 @@ export default class Workspace {
 
     initializeConnectionMonitoring() {
         // Get the underlying Pusher/Socket connection
-        const connector = this.echo?.connector;
-
-        if (!connector) {
-            this.debug('⚠️ Connection monitoring not available - no connector');
-            return;
-        }
+        const connector = this.echo.connector;
 
         if (connector.pusher) {
             // Pusher-based connection (Pusher, Reverb, Soketi, etc.)
